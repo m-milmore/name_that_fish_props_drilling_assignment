e22a97b7d79758783b88823d5423ba7fab280d2d
m-milmore <38921020+m-milmore@users.noreply.github.com>
2023-04-04 08:39:48 -0400

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { GameBoard } from "./Components/GameBoard";
+import { ScoreBoard } from "./Components/ScoreBoard";
+import "./Components/styles/final-score.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <ScoreBoard />
+        <GameBoard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
