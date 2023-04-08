21c98d5fd5b1c7aed45f231009d0a81a2ccfa0e4
m-milmore <38921020+m-milmore@users.noreply.github.com>
2023-04-08 14:30:34 -0400

    problem solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,59 @@
-import "./App.css";
+import { useState } from "react";
 import { GameBoard } from "./Components/GameBoard";
 import { ScoreBoard } from "./Components/ScoreBoard";
+import { FinalScore } from "./Components/FinalScore";
+import { Images } from "./assets/images";
+import "./App.css";
 import "./Components/styles/final-score.css";
 
 function App() {
+  const initialFishes = [
+    {
+      name: "trout",
+      url: Images.trout,
+    },
+    {
+      name: "salmon",
+      url: Images.salmon,
+    },
+    {
+      name: "tuna",
+      url: Images.tuna,
+    },
+    {
+      name: "shark",
+      url: Images.shark,
+    },
+  ];
+  const [correctCount, setCorrectCount] = useState(0);
+  const [incorrectCount, setIncorrectCount] = useState(0);
+  const [displayFinalScore, setDisplayFinalScore] = useState(false);
+  const [answersLeft, setAnswersLeft] = useState(() =>
+    initialFishes.map((fish) => fish.name)
+  );
+
   return (
     <div className="App">
       <header>
-        <ScoreBoard />
-        <GameBoard />
+        <ScoreBoard
+          correctCount={correctCount}
+          incorrectCount={incorrectCount}
+          answersLeft={answersLeft}
+        />
+        {displayFinalScore ? (
+          <FinalScore
+            correctCount={correctCount}
+            totalCount={initialFishes.length}
+          />
+        ) : (
+          <GameBoard
+            setCorrectCount={setCorrectCount}
+            setIncorrectCount={setIncorrectCount}
+            setDisplayFinalScore={setDisplayFinalScore}
+            initialFishes={initialFishes}
+            setAnswersLeft={setAnswersLeft}
+          />
+        )}
       </header>
     </div>
   );
--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -1,37 +1,54 @@
+import { useState } from "react";
 import "./styles/game-board.css";
-import { Images } from "../assets/images";
 
-const initialFishes = [
-  {
-    name: "trout",
-    url: Images.trout,
-  },
-  {
-    name: "salmon",
-    url: Images.salmon,
-  },
-  {
-    name: "tuna",
-    url: Images.tuna,
-  },
-  {
-    name: "shark",
-    url: Images.shark,
-  },
-];
+export const GameBoard = ({
+  setCorrectCount,
+  setIncorrectCount,
+  setDisplayFinalScore,
+  initialFishes,
+  setAnswersLeft,
+}) => {
+  const [nextFishToName, setNextFishToName] = useState(initialFishes[0]);
+  const [fishGuess, setFishGuess] = useState("");
 
-export const GameBoard = () => {
-  const nextFishToName = initialFishes[0];
+  const handleOnChange = ({ target: { value } }) => {
+    setFishGuess(value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setAnswersLeft((prev) =>
+      prev.filter((fish) => fish !== nextFishToName.name)
+    );
+    nextFishToName.name === fishGuess
+      ? setCorrectCount((prev) => (prev += 1))
+      : setIncorrectCount((Prev) => (Prev += 1));
+    const currentIndex = initialFishes.findIndex(
+      ({ name }) => name === nextFishToName.name
+    );
+    if (currentIndex + 1 < initialFishes.length)
+      setNextFishToName(initialFishes[currentIndex + 1]);
+    else {
+      setNextFishToName(initialFishes[0]);
+      setDisplayFinalScore(true);
+    }
+    setFishGuess("");
+  };
 
   return (
     <div id="game-board">
       <div id="fish-container">
         <img src={nextFishToName.url} alt={nextFishToName.name} />
       </div>
-      <form id="fish-guess-form" onSubmit={(e) => {}}>
+      <form id="fish-guess-form" onSubmit={handleSubmit}>
         <label htmlFor="fish-guess">What kind of fish is this?</label>
-        <input type="text" name="fish-guess" />
-        <input type="submit" />
+        <input
+          type="text"
+          name="fish-guess"
+          value={fishGuess}
+          onChange={handleOnChange}
+        />
+        <input type="submit" style={{ cursor: "pointer" }} />
       </form>
     </div>
   );
